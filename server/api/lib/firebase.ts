155d182c43bab430064b51405f6ebf56f53ea932
gmth6789 <143313748+gmth6789@users.noreply.github.com>
2023-09-08 16:17:48 +0700

    1 file modified, 1 added

--- server/api/lib/firebase.ts
+++ server/api/lib/firebase.ts
@@ -1,13 +1,15 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
-
-
-
-
-
-
-
+var firebaseConfig = {
+  apiKey: "AIzaSyAKpe9M-QP6wD0OFmHEkQo5oE8fnvIVVNI",
+  authDomain: "app-1-45f78.firebaseapp.com",
+  projectId: "app-1-45f78",
+  storageBucket: "app-1-45f78.appspot.com",
+  messagingSenderId: "167089103997",
+  appId: "1:167089103997:web:fed8dd46d61c41a7da7d14",
+  measurementId: "G-KHG10CEL2W"
+};
 
 
 export const firebaseApp = initializeApp(firebaseConfig);
